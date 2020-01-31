136176d2735f67f0ed41586161caf94be9da7fe4
Sandy <sandychn@163.com>
2020-01-31 23:58:28 +0800

    update leetcode header

--- LeetCode/leetcode_header.hpp
+++ LeetCode/leetcode_header.hpp
@@ -136,3 +136,20 @@ TreeNode* stringToTreeNode(string input) {
 }
 
 string boolToString(bool input) { return input ? "True" : "False"; }
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
