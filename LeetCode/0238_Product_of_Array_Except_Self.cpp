b81d32f84840f151338fa5a0fdbf4be956d9c4a2
Sandy <sandychn@163.com>
2020-03-09 09:07:32 +0800

    update leetcode 238

---
+++ LeetCode/0238_Product_of_Array_Except_Self.cpp
@@ -0,0 +1,23 @@
+// Time  : 12 ms
+// Memory: 17.9 MB
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        const int n = nums.size();
+        vector<int> res(n);
+        for (int i = 0; i < n; i++) {
+            if (i) res[i] = res[i - 1] * nums[i];
+            else res[i] = nums[i];
+        }
+        for (int i = n - 1, right = 1; i >= 0; i--) {
+            if (i) {
+                res[i] = res[i - 1] * right;
+                right *= nums[i];
+            } else {
+                res[i] = right;
+            }
+        }
+        return res;
+    }
+};
