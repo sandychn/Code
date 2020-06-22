de777ad5614313bc75ae2fa50cd761ffdc175b34
Sandy <sandychn@163.com>
2020-06-23 01:55:56 +0800

    remove leetcode from this repo

--- LeetCode/0796_Rotate_String.cpp
+++
@@ -1,9 +0,0 @@
-// Time  : 0 ms (100.00%)
-// Memory: 8.4 MB (40.21%)
-
-class Solution {
-public:
-    bool rotateString(string A, string B) {
-        return A.length() == B.length() && (A + A).find(B) != string::npos;
-    }
-};
